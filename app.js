f1f2f6693701546ffeeb5f2573f22ff34406eaaa
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2023-09-24 07:52:01 +0300

    Exercise 11.11., add an endpoint for a version string

--- app.js
+++ app.js
@@ -7,6 +7,10 @@ app.get('/health', (req, res) => {
   res.send('ok')
 })
 
+app.get('/version', (req, res) => {
+  res.send('1') // change this string to ensure a new version deployed
+})
+
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
