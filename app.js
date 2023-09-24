01ec79bbcdc0472be61cff2aaa79577e392dc701
Jari Ikonen <87890204+jariikonen@users.noreply.github.com>
2023-09-24 07:15:01 +0300

    Exercise 11.11., add a failing http check to fly.toml

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 const express = require('express')
 const app = express()
 
+app.get('/health', (req, res) => {
+  throw 'error...'
+  // eslint-disable-next-line no-unreachable
+  res.send('ok')
+})
+
 // Heroku dynamically sets a port
 const PORT = process.env.PORT || 5000
 
